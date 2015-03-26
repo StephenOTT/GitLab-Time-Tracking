f1be35b7a8a368687c821a2b88c17e5c5af5cda3
Stephen Russett <stephenrussett@gmail.com>
2015-03-26 01:52:29 -0400

    added date correction code for mongodb formatting

--- app/gitlab_downloader.rb
+++ app/gitlab_downloader.rb
@@ -4,6 +4,7 @@ require_relative 'issues'
 require_relative 'mongo_connection'
 require 'awesome_print'
 # require 'secure_random'
+# require 'time'
 
 class GitLab_Downloader
 
@@ -49,6 +50,11 @@ class GitLab_Downloader
 			# Iterates through each issue and get the notes and merges the notes into the issue hash.
 			issues.each do |x|
 				x = x.to_h	# Converts the ObjectifiedHash that is returned by GitLab into a Ruby Hash
+				
+				# x["created_at"] = Time.parse(x["created_at"]).strftime("%FT%T")
+				x["created_at"] = DateTime.strptime(x["created_at"], '%Y-%m-%dT%H:%M:%S.%L%z').to_time.utc
+				x["updated_at"] = DateTime.strptime(x["updated_at"], '%Y-%m-%dT%H:%M:%S.%L%z').to_time.utc
+
 				commentPageNum = 1
 				# issueComments = @glClient.issue_notes(x["project_id"], x["id"])	# Gets the notes for the current issue
 				issueComments = @glClient.issue_notes(x["project_id"], x["id"], :per_page=>100, :page=>commentPageNum)	# Gets the notes for the current issue
@@ -59,7 +65,9 @@ class GitLab_Downloader
 					while issueComments.length > 0  do
 						issueComments.each do |y|
 							y = y.to_h
-							
+
+							y["created_at"] = DateTime.strptime(y["created_at"], '%Y-%m-%dT%H:%M:%S.%L%z').to_time.utc
+
 							# Parses each comment for Time Tracking Information. Then merges back into the Comment Hash
 							timeTrack = Gl_Issue.process_comment(y)
 							if timeTrack != nil
@@ -72,6 +80,10 @@ class GitLab_Downloader
 						commentPageNum += 1
 						issueComments = @glClient.issue_notes(x["project_id"], x["id"], :per_page=>100, :page=>commentPageNum)	# Gets the notes for the current issue
 					end
+
+					if x["milestone"] != nil
+						x["milestone"]["created_at"] = DateTime.strptime(x["milestone"]["created_at"], '%Y-%m-%dT%H:%M:%S.%L%z').to_time.utc
+						x["milestone"]["updated_at"] = DateTime.strptime(x["milestone"]["updated_at"], '%Y-%m-%dT%H:%M:%S.%L%z').to_time.utc
 					
 					x["admin_info"] = add_admin_records
 					
