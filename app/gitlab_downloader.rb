5c02e826d6ebe9e942302ded04c434b5c79e94ff
Stephen Russett <stephenrussett@gmail.com>
2015-03-29 19:51:17 -0400

    refactor to cleanup code + addition of downloading of project info

--- app/gitlab_downloader.rb
+++ app/gitlab_downloader.rb
@@ -3,7 +3,7 @@ require 'gitlab'
 require_relative 'issues'
 require_relative 'milestones'
 require_relative 'mongo_connection'
-require 'awesome_print'
+# require 'awesome_print'
 # require 'secure_random'
 # require 'time'
 
@@ -37,15 +37,17 @@ class GitLab_Downloader
 		downloadID = SecureRandom.uuid
 
 		return adminRecords = { "download_timestamp" => creationTime,
-							"downloaded_by_user" => creationUser["username"],
-							"downloaded_by_user_id" => creationUser["id"],
-							"gitlab_endpoint" => gitlabEndpoint,
-							"download_id" => downloadID
-						}
+								"downloaded_by_user" => creationUser["username"],
+								"downloaded_by_user_id" => creationUser["id"],
+								"gitlab_endpoint" => gitlabEndpoint,
+								"download_id" => downloadID
+								}
 	end
 
 	def downloadIssuesAndComments(projectID)
-		# issues = @glClient.issues(153287)
+		projectInfo = @glClient.project(projectID)
+		projectInfo = projectInfo.to_h
+		
 		issuePageNum = 1
 		issues = @glClient.issues(projectID, :per_page=>100, :page=>issuePageNum)
 		issues2 = []
@@ -55,54 +57,57 @@ class GitLab_Downloader
 			issues.each do |x|
 				x = x.to_h	# Converts the ObjectifiedHash that is returned by GitLab into a Ruby Hash
 				
-				# x["created_at"] = Time.parse(x["created_at"]).strftime("%FT%T")
 				x["created_at"] = DateTime.strptime(x["created_at"], '%Y-%m-%dT%H:%M:%S.%L%z').to_time.utc
 				x["updated_at"] = DateTime.strptime(x["updated_at"], '%Y-%m-%dT%H:%M:%S.%L%z').to_time.utc
 
 				commentPageNum = 1
-				# issueComments = @glClient.issue_notes(x["project_id"], x["id"])	# Gets the notes for the current issue
 				issueComments = @glClient.issue_notes(x["project_id"], x["id"], :per_page=>100, :page=>commentPageNum)	# Gets the notes for the current issue
 
-				if issueComments.length > 0
-					comments2 = []
 
-					while issueComments.length > 0  do
-						issueComments.each do |y|
-							y = y.to_h
+				if issueComments.length > 0	# If there are notes in the issue then...
+					comments2 = []	# Array used to hold comments with time tracking information.
+
+
+					while issueComments.length > 0  do  # Loop used to control api pagination
+						issueComments.each do |y|	# For Each Comment...
+							y = y.to_h	# Convert from ObjectifiableHash to Ruby Hash
 
 							y["created_at"] = DateTime.strptime(y["created_at"], '%Y-%m-%dT%H:%M:%S.%L%z').to_time.utc
 
 							# Parses each comment for Time Tracking Information. Then merges back into the Comment Hash
 							timeTrack = Gl_Issue.process_comment(y)
-							if timeTrack != nil
-								y = y.merge(timeTrack)
+							if timeTrack != nil	 # If Time Tracking data was found for the Comment then....
+								y = y.merge(timeTrack) # Merge time tracking hash into Comment Hash
 
-							comments2 << y
-
+								comments2 << y   # Add the merged hash into the Comment2 Array which holds comments that have time tracking information
 							end
 						end
+
+						# Part of the pagination process to go through as many pages of comments that may exist for the current issue
 						commentPageNum += 1
 						issueComments = @glClient.issue_notes(x["project_id"], x["id"], :per_page=>100, :page=>commentPageNum)	# Gets the notes for the current issue
 					end
 
-					if x["milestone"] != nil
-						x["milestone"]["created_at"] = DateTime.strptime(x["milestone"]["created_at"], '%Y-%m-%dT%H:%M:%S.%L%z').to_time.utc
-						x["milestone"]["updated_at"] = DateTime.strptime(x["milestone"]["updated_at"], '%Y-%m-%dT%H:%M:%S.%L%z').to_time.utc
-						x["milestone"]["due_date"] = DateTime.strptime(x["milestone"]["due_date"], '%Y-%m-%d').to_time.utc
-
-
-						milestoneBudgetTrack = Gl_Milestone.process_milestone(x["milestone"])
-						if milestoneBudgetTrack != nil
-							x["milestone"]["milestone_budget_data"] = milestoneBudgetTrack
-						end
-					end
-					
-					x["admin_info"] = add_admin_records
-					
-					x["comments"] = comments2 	# Merges the comments/notes into the main Issues Hash for each issue
-					
+					# if there are comments with time tracking information then....
 					if comments2.length > 0
-						x["created_at"] = x["created_at"]
+											# If there is milestone data then....
+						if x["milestone"] != nil
+							x["milestone"]["created_at"] = DateTime.strptime(x["milestone"]["created_at"], '%Y-%m-%dT%H:%M:%S.%L%z').to_time.utc
+							x["milestone"]["updated_at"] = DateTime.strptime(x["milestone"]["updated_at"], '%Y-%m-%dT%H:%M:%S.%L%z').to_time.utc
+							x["milestone"]["due_date"] = DateTime.strptime(x["milestone"]["due_date"], '%Y-%m-%d').to_time.utc
+
+
+							milestoneBudgetTrack = Gl_Milestone.process_milestone(x["milestone"])	# Process the milestone data for budget tracking information.
+							if milestoneBudgetTrack != nil
+								x["milestone"]["milestone_budget_data"] = milestoneBudgetTrack
+							end
+						end
+
+						x["project_info"] = projectInfo
+
+						x["admin_info"] = add_admin_records
+						x["comments"] = comments2 	# Merges the comments/notes into the main Issues Hash for each issue
+
 						issues2 << x
 					end
 				end
