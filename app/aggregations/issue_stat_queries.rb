ab6784b446f690ad3171bb14659848dd4653f3c9
Stephen Russett <stephenrussett@gmail.com>
2015-04-05 23:10:22 -0400

    Added queries for Analyze

--- app/aggregations/issue_stat_queries.rb
+++ app/aggregations/issue_stat_queries.rb
@@ -22,7 +22,17 @@ class Issue_Stat_Queries
 							])
 	end
 
-
+	def get_project_snapshot_info(downloadID)
+		# TODO add filtering and extra security around query
+		output = @mongoConnection.aggregate([
+			# { "$match" => { project_id: projectID }},
+			{"$limit" => 1},
+			{"$project" => {_id: 0,
+							admin_info: "$admin_info", 
+							project_info: "$project_info",
+							}}
+							]).first
+	end
 
 
 	def get_issues_time
@@ -119,8 +129,122 @@ class Issue_Stat_Queries
 	end
 
 
+	def get_issues_for_milestone(downloadID, milestoneNumber)
+		output = @mongoConnection.aggregate([
+			{ "$unwind" => "$comments" },
+			{"$project" => {_id: 0, 
+							download_id: "$admin_info.download_id",
+							# project: "$project_info.path_with_namespace",
+							issue_number: "$iid",
+							issue_title: "$title",
+							issue_state: "$state",
+							comment_duration: "$comments.time_tracking_data.duration",
+							comment_time_comment: { "$ifNull" => [ "$comments.time_tracking_data.time_comment", nil ] },
+							milestone_number: { "$ifNull" => [ "$milestone.iid", nil ] },
+							}},
+			{ "$match" => {download_id: downloadID}},
+			{ "$match" => {milestone_number: milestoneNumber}},
+			{ "$group" => { _id: {
+								# project: "$project",
+								issue_number: "$issue_number",
+								issue_title: "$issue_title",
+								issue_state: "$issue_state",
+								},
+								time_duration_sum: { "$sum" => "$comment_duration" },
+								time_comment_count: {"$sum" => 1}
+								}
+							},
+							])
+		output.each do |x|
+			x["_id"]["time_duration_sum"] = x["time_duration_sum"]
+			x["_id"]["time_comment_count"] = x["time_comment_count"]
+		end
+		return output.map { |e| e["_id"]  }
+	end
 
-	
+	def get_milestones(downloadID)
+		output = @mongoConnection.aggregate([
+			# { "$unwind" => "$comments" },
+			{"$project" => {_id: 0, 
+							download_id: "$admin_info.download_id",
+							project: "$project_info.path_with_namespace",
+							# issue_number: "$iid",
+							# issue_title: "$title",
+							# issue_state: "$state",
+							# comment_duration: "$comments.time_tracking_data.duration",
+							# comment_time_comment: { "$ifNull" => [ "$comments.time_tracking_data.time_comment", nil ] },
+							milestone_number: { "$ifNull" => [ "$milestone.iid", nil ] },
+							milestone_title: { "$ifNull" => [ "$milestone.title", nil ] },
+							milestone_budget_comment: { "$ifNull" => [ "$milestone.milestone_budget_data.budget_comment", nil ] },
+							milestone_state: { "$ifNull" => [ "$milestone.state", nil ] },
+							milestone_due_date: { "$ifNull" => [ "$milestone.due_date", nil ] },
+							milestone_budget_duration: { "$ifNull" => [ "$milestone.milestone_budget_data.duration", nil ] },
+
+
+							}},
+			{ "$match" => {download_id: downloadID}},
+			{ "$group" => { _id: {
+								download_id: "$download_id",
+								project: "$project",
+								milestone_number: "$milestone_number",
+								milestone_title: "$milestone_title",
+								milestone_budget_comment: "$milestone_budget_comment",
+								milestone_state: "$milestone_state",
+								milestone_due_date: "$milestone_due_date",
+								milestone_budget_duration: "$milestone_budget_duration"
+
+								}}
+							},
+							])
+		# output.each do |x|
+		# 	x["_id"]["time_duration_sum"] = x["time_duration_sum"]
+		# 	x["_id"]["time_comment_count"] = x["time_comment_count"]
+		# end
+		return output.map { |e| e["_id"]  }
+	end
+	def get_milestone_sums(downloadID, milestoneNumber)
+		output = @mongoConnection.aggregate([
+			{ "$unwind" => "$comments" },
+			{"$project" => {_id: 0, 
+							download_id: "$admin_info.download_id",
+							project: "$project_info.path_with_namespace",
+							issue_number: "$iid",
+							# issue_title: "$title",
+							# issue_state: "$state",
+							comment_duration: "$comments.time_tracking_data.duration",
+							# comment_time_comment: { "$ifNull" => [ "$comments.time_tracking_data.time_comment", nil ] },
+							milestone_number: { "$ifNull" => [ "$milestone.iid", nil ] },
+							milestone_title: { "$ifNull" => [ "$milestone.title", nil ] },
+							milestone_budget_comment: { "$ifNull" => [ "$milestone.milestone_budget_data.budget_comment", nil ] },
+							milestone_state: { "$ifNull" => [ "$milestone.state", nil ] },
+							milestone_due_date: { "$ifNull" => [ "$milestone.due_date", nil ] },
+							milestone_budget_duration: { "$ifNull" => [ "$milestone.milestone_budget_data.duration", nil ] },
+
+
+							}},
+			{ "$match" => {download_id: downloadID, milestone_number: milestoneNumber}},
+			{ "$group" => { _id: {
+								download_id: "$download_id",
+								# project: "$project",
+								milestone_number: "$milestone_number",
+								# milestone_title: "$milestone_title",
+								# milestone_budget_comment: "$milestone_budget_comment",
+								# milestone_state: "$milestone_state",
+								# milestone_due_date: "$milestone_due_date",
+								# milestone_budget_duration: "$milestone_budget_duration",
+								# comment_duration: "$comment_duration",
+
+								},
+								time_duration_sum: { "$sum" => "$comment_duration" },
+								time_comment_count: {"$sum" => 1}
+							}},
+							])
+		output.each do |x|
+			x["_id"]["time_duration_sum"] = x["time_duration_sum"]
+			x["_id"]["time_comment_count"] = x["time_comment_count"]
+		end
+		return output.map { |e| e["_id"]  }.first
+	end
 
 end
 
@@ -128,5 +252,11 @@ end
 # m = Mongo_Connection.new("localhost", 27017, "GitLab", "Issues_Time_Tracking") 
 # output = Issue_Stat_Queries.new(m)
 
+
 # ap output.get_issues_time
 # ap output.get_all_issues_time("abdd1c36-4cb0-4828-bbe0-34a1d679ca2f")
+# ap output.get_issues_for_milestone("57a364cb-9fb2-423c-a1aa-72ede9934325", 1)
+# ap output.get_milestones("8cec1dc7-2284-46d4-937b-b5ae937bf2c9", 1)
+# ap output.get_milestone_sums("8cec1dc7-2284-46d4-937b-b5ae937bf2c9", 1)
+# ap output.get_project_snapshot_info("8cec1dc7-2284-46d4-937b-b5ae937bf2c9")
+
